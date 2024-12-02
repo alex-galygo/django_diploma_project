79d77578d76bc4437f867d9e5f3dcac1b130e2fe
Alex Galygo <alex.galygo.94@gmail.com>
2024-12-02 22:15:30 +0300

    add class and html templates

--- apps/tasks/urls.py
+++ apps/tasks/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path, include
 from . import views
+from .views import AddTask, TaskEditView, TaskDeleteView, TaskView, TaskListView
 
 urlpatterns = [
-    path('create/', views.create_task, name='create_task'),
+    path('create/', AddTask.as_view(), name='create_task'),
 
-    path('', views.task_list, name='index'),
+    path('', views.TaskListView.as_view(), name='index'),
+
+    path('task/<int:pk>/', TaskView.as_view(), name='task_detail'),
+
+    path('task/<int:pk>/edit/', TaskEditView.as_view(), name='task_edit'),
+
+    path('task/<int:pk>/delete/', views.TaskDeleteView.as_view(), name='task_delete'),
 
 ]
--- apps/tasks/views.py
+++ apps/tasks/views.py
@@ -1,30 +1,80 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
+from django.urls import reverse_lazy
 
 from .forms import TaskForm, Task
 
 
-@login_required
-def create_task(request):
-    if request.method == 'POST':
-        form = TaskForm(request.POST)
-        if form.is_valid():
-            task = form.save(commit=False)
-            task.user = request.user
-            task.save()
-            messages.success(request, 'Задача успешно создана')
-            return redirect('task_list')
-    else:
-        form = TaskForm()
+class AddTask(LoginRequiredMixin, CreateView):
+    model = Task
+    form_class = TaskForm
+    template_name = 'create_task.html'
+    success_url = 'task_list'
 
-    return render(request, 'create_task.html', {'form': form})
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
 
 
-def task_list(request):
-    tasks = Task.objects.all()
-    context = {
-        'tasks': tasks
-    }
-    return render(request, 'task_list.html', context)
+class TaskListView(ListView):
+    model = Task
+    template_name = 'task_list.html'
+    context_object_name = 'tasks'
 
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return super().get(request, *args, **kwargs)
+        return render(request, 'index.html')
+
+    def get_queryset(self):
+        return Task.objects.filter(user=self.request.user)
+
+
+class TaskView(LoginRequiredMixin, UserPassesTestMixin, DetailView):
+    model = Task
+    template_name = 'task_detail.html'
+    context_object_name = 'task'
+
+    def test_func(self):
+        task = self.get_object()
+        return task.user == self.request.user
+
+    def handle_no_permission(self):
+        messages.error(self.request, "У вас нет прав для просмотра этой задачи")
+        return redirect('index')
+
+    def get_success_url(self):
+        return reverse_lazy('task_detail', kwargs={'pk': self.object.pk})
+
+
+class TaskEditView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Task
+    form_class = TaskForm
+    template_name = 'task_edit.html'
+
+    def test_func(self):
+        task = self.get_object()
+        return task.user == self.request.user
+
+    def handle_no_permission(self):
+        messages.error(self.request, "У вас нет прав для редактирования этой задачи")
+        return redirect('index')
+
+    def get_success_url(self):
+        return reverse_lazy('task_detail', kwargs={'pk': self.object.pk})
+
+
+class TaskDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Task
+    template_name = 'task_delete.html'
+    success_url = reverse_lazy('task_list')
+
+    def test_func(self):
+        task = self.get_object()
+        return task.user == self.request.user
+
+    def handle_no_permission(self):
+        messages.error(self.request, "У вас нет прав для удаления этой задачи")
+        return redirect('index')
